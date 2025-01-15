b362bd55d12b1a80a000ddd4144a861b304893e5
Aldrick Bonaobra <git@aldrickb.com>
2025-01-15 18:38:43 +0800

    Add thirdweb v5

---
+++ app/client.tsx
@@ -0,0 +1,39 @@
+import  { createThirdwebClient, defineChain, getContract } from "thirdweb";
+
+// Replace this with your client ID string
+// refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
+const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
+
+// @dev - This is the format for a custom chain definition. 
+// Replace this with your own chain configuration if you're using a different network.
+const customChain = defineChain({
+    id: 0,
+    name: "Blockchain Name",
+    nativeCurrency: { name: "Token Name", symbol: "Token Symbol", decimals: 18 },
+    icon: {
+        url: "https://example.com/url-to-your-chain-icon/chainicon.png",
+        width: 500,
+        height: 500,
+        format: "png"
+    },
+    rpc: "https://url-to-your-rpc-endpoint.example.com",
+    testnet: true,
+    blockExplorers: [
+      {
+        name: "Your Explorer Name",
+        url: "https://url-to-your-explorer.example.com",
+        apiUrl: "https://url-to-your-explorer-api.example.com/api",
+      },
+    ],
+});
+
+if (!clientId) {
+  throw new Error("No client ID provided");
+}
+
+export const client = createThirdwebClient({
+  clientId: clientId,
+});
+
+// @dev - Uncomment the following line to use your custom chain definition. This will override the default chains available in thirdweb.
+// export const chain = customChain
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import { ConnectButton, useActiveAccount } from "thirdweb/react";
+import { client } from "./client";
+import { arbitrumSepolia } from "thirdweb/chains";
+
 
 export default function Home() {
   return (
@@ -46,6 +50,32 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             Read our docs
+          </a>
+        </div>
+
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)] mt-4">
+          <li className="mb-2">Test Thirdweb Integration below.</li>
+          <li>You will be connect to Arbitrum Sepolia.</li>
+        </ol>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <ConnectButton 
+            connectButton={{
+              className: "w-full"
+            }}
+            detailsButton={{
+              className: "!w-full justify-center"
+            }}
+            client={client} 
+            chain={arbitrumSepolia} 
+          />
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://portal.thirdweb.com/react/v5/getting-started"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Thirdweb docs
           </a>
         </div>
       </main>
