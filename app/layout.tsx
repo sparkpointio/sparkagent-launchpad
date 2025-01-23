88eab8a3b7922a2c5bc2fb880c585632ebe2510e
Kevin Kyle Hendrick Baldo <30228996+kbaldo@users.noreply.github.com>
2025-01-23 15:32:32 +0800

    Configure fonts for initial text styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Rubik, Poppins } from "next/font/google";
 import "./globals.css";
 import { ThirdwebProvider } from "thirdweb/react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const rubik = Rubik({
+  weight: '400',
+  variable: '--font-rubik',
+  subsets: ['latin']
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const poppins = Poppins({
+  weight: '400',
+  variable: '--font-poppins',
+  subsets: ['latin']
 });
 
 export const metadata: Metadata = {
@@ -33,7 +35,7 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 
         {/* Apple Touch Icon */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1" />
 
         {/* Android Chrome Icons */}
         <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
@@ -49,7 +51,7 @@ export default function RootLayout({
       </head>
       <ThirdwebProvider>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${rubik.variable} ${poppins.variable} antialiased`}
         >
           {children}
         </body>
