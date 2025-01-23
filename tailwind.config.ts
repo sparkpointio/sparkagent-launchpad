85c848ab2f3a215064d6496a8bbe30a6408aa9c0
Kevin Kyle Hendrick Baldo <30228996+kbaldo@users.noreply.github.com>
2025-01-23 15:52:27 +0800

    Configure initial brand colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,30 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        sparkyOrange: {
+          DEFAULT: '#FFC94D',
+          100: '#FFF8DB', // Lighter shade
+          200: '#FFF0B7',
+          300: '#FFE594',
+          400: '#FFDA79',
+          500: '#FFC94D', // Main color
+          600: '#DBA438', // Darker shade
+          700: '#B78226',
+          800: '#936218',
+          900: '#7A4C0E',
+        },
+        sparkyGreen: {
+          DEFAULT: '#00D7B2',
+          100: '#CAFDE1', // Lighter shade
+          200: '#96FBCD',
+          300: '#61F3BE',
+          400: '#39E7B8',
+          500: '#00D7B2', // Main color
+          600: '#00B8AA', // Darker shade
+          700: '#00989A',
+          800: '#006E7C',
+          900: '#005267',
+        },
       },
     },
   },
