88b6837636750032e15e74149dfeb00bb83d1fcd
Kevin Kyle Hendrick Baldo <30228996+kbaldo@users.noreply.github.com>
2025-01-24 22:14:01 +0800

    Improve visibility of elements in other display sizes

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,16 +6,16 @@ import Link from 'next/link';
 
 const Hero = () => {
   return (
-    <section className="text-center relative w-full h-[60vh]">
+    <section className="text-center relative w-full h-[60vh] sm:h-[75vh] m:h-[60vh]">
       <div className="bg-cover bg-center bg-no-repeat absolute top-0 left-0 w-full h-full z-0 bg-img-1" style={{ backgroundImage: `url(${bg_hero_dark.src})` }}></div>
       {/* <div className="bg-cover bg-center bg-no-repeat absolute top-0 left-0 w-full h-full z-0 bg-img-2" style={{ backgroundImage: `url(${bg_hero_dark.src})` }}></div> */}
       <div className="absolute top-0 left-0 w-full h-full z-1 bg-mask"></div>
 
       <div className='m-auto max-w-4xl px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center h-full relative z-2'>
-        <h1 className="text-4xl md:text-6xl leading-tight text-center mt-24 text-white">
+        <h1 className="text-4xl xl:text-5xl 2xl:text-6xl leading-tight text-center mt-24 text-white">
           No-Code, Create<br/> Co-own AI Agents
         </h1>
-        <p className="mx-auto mt-6 max-w-2xl text-white md:text-2xl">
+        <p className="mx-auto mt-6 max-w-2xl text-white text-xl lg:text-2xl">
           Tokenize your ideas into AI agents that collaborate, trade, and earn for you!
         </p>
         <div className="mt-10 flex flex-col sm:flex-row justify-center gap-4">
