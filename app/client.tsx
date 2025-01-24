d173b76c9716d1bd0c89269f044d77aaaaf986d3
Harvey Javier <harveyjavier17@gmail.com>
2025-01-24 23:20:15 +0800

    Fix unused vars lint error

--- app/client.tsx
+++ app/client.tsx
@@ -1,4 +1,4 @@
-import  { createThirdwebClient, defineChain, getContract } from "thirdweb";
+import  { createThirdwebClient /*, defineChain, getContract*/ } from "thirdweb";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
@@ -6,7 +6,7 @@ const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
 
 // @dev - This is the format for a custom chain definition. 
 // Replace this with your own chain configuration if you're using a different network.
-const customChain = defineChain({
+/*const customChain = defineChain({
     id: 0,
     name: "Blockchain Name",
     nativeCurrency: { name: "Token Name", symbol: "Token Symbol", decimals: 18 },
@@ -25,7 +25,7 @@ const customChain = defineChain({
         apiUrl: "https://url-to-your-explorer-api.example.com/api",
       },
     ],
-});
+});*/
 
 if (!clientId) {
   throw new Error("No client ID provided");
