086682c50cd24a17aef49d3b4142a57f0dd3a82a
Kevin Kyle Hendrick Baldo <30228996+kbaldo@users.noreply.github.com>
2025-01-27 20:45:22 +0800

    Create new agent page

---
+++ app/agent/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+"use client";
+import React from "react";
+import { AGENTS } from "../lib/utils/agents-sample-data";
+import AgentStats from "../components/AgentStats";
+import SwapCard from "../components/SwapCard";
+
+export default function Agent() {
+	return (
+		<div className="items-center justify-center min-h-screen m-6 lg:mx-2 xl:mx-10 2xl:mx-24">
+			<div className="grid grid-cols-1 lg:grid-cols-3 gap-4 w-full">
+				
+				{/* First Column */}
+				<div className="lg:col-span-2 w-full">
+          <AgentStats
+            title={AGENTS[1].title}
+            image={AGENTS[1].image}
+            imageAlt={AGENTS[1].title}
+            certificate={AGENTS[1].certificate}
+            description={AGENTS[1].description}
+            createdBy={AGENTS[1].createdBy}
+            marketCap={AGENTS[1].marketCap}
+            datePublished={AGENTS[1].datePublished}
+            sparkingProgress={AGENTS[1].sparkingProgress}
+          />
+				</div>
+
+        {/* Second Column */}
+				<div className="flex flex-col">
+					<SwapCard />
+				</div>
+			</div>
+		</div>
+	);
+}
