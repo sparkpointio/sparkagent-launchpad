64daefa5b6e10fdd988264483ba3b45796198ddc
Kevin Kyle Hendrick Baldo <30228996+kbaldo@users.noreply.github.com>
2025-01-29 16:07:08 +0800

    Match dark mode styling in swap buttons with search bar button

--- app/components/SwapCard.tsx
+++ app/components/SwapCard.tsx
@@ -5,7 +5,7 @@ import { new_sparkpoint_logo } from "../lib/assets";
 
 const SwapCard = () => {
     const headerProperties = "flex text-xl justify-right";
-    const buttonProperties = "w-full border-2 border-black dark:border-gray-700 rounded-3xl hover:bg-sparkyGreen-500 transition-all p-4";
+    const buttonProperties = "w-full border-2 border-black dark:border-gray-600 dark:bg-gray-800 rounded-3xl hover:bg-sparkyGreen-500 dark:hover:bg-sparkyGreen-600 transition-all p-4";
     return (
         <div className="bg-white dark:bg-[#1a1d21] dark:text-white h-min border-2 border-black rounded-2xl shadow-md p-5 m:p-6">
             <h2 className={`${headerProperties} font-bold mb-4`}>Swap</h2>
@@ -13,7 +13,7 @@ const SwapCard = () => {
                 <button type="button" className={buttonProperties}> Buy </button>
                 <button type="button" className={buttonProperties}> Sell </button>
             </div>
-            <div className="relative flex items-center bg-gray-200 dark:bg-gray-700 p-2 rounded-lg mb-12">
+            <div className="relative flex items-center bg-gray-200 dark:placeholder-gray-400 dark:bg-gray-700 p-2 rounded-lg mb-12">
                 <input
                     type="text"
                     title="Enter amount"
