f4737b3d77acf7435f95f524a181fcfe173baea8
Kevin Kyle Hendrick Baldo <30228996+kbaldo@users.noreply.github.com>
2025-01-31 00:58:10 +0800

    Make geckoterminal iframe dynamic

--- app/components/AgentStats.tsx
+++ app/components/AgentStats.tsx
@@ -198,7 +198,7 @@ const AgentStats: React.FC<AgentStatsProps> = ({
 				width="100%"
 				id="geckoterminal-embed"
 				title="GeckoTerminal Embed"
-				src={`https://www.geckoterminal.com/arbitrum/pools/${"0xc6f780497a95e246eb9449f5e4770916dcd6396a"}?embed=1&info=0&swaps=0&grayscale=0&light_chart=${isDarkMode ? '0' : '1'}`}
+				src={`https://www.geckoterminal.com/sepolia-testnet/pools/${certificate}?embed=1&info=0&swaps=0&grayscale=0&light_chart=${isDarkMode ? '0' : '1'}`}
 			></iframe>
 		</motion.div>
 	);
