8be5a7443e82dbd2247fc360df8d27532e710f3f
Kevin Kyle Hendrick Baldo <30228996+kbaldo@users.noreply.github.com>
2025-02-03 22:31:47 +0800

    Add pagination

--- app/components/Agents.tsx
+++ app/components/Agents.tsx
@@ -16,11 +16,15 @@ import { useReadContract } from "thirdweb/react";
 import { getContract, toEther } from "thirdweb";
 import { arbitrumSepolia } from "thirdweb/chains";
 import SparkAgentLogo from "./SparkAgentLogo";
+import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
 
 const Agents = () => {
     //const [agents, setAgents] = useState<string[]>([]);
     const [index, setIndex] = useState(0);
     const [address, setAddress] = useState<string>("");
+    const [currentPage, setCurrentPage] = useState(1);
+    const agentsPerPage = 6;
+
     interface AgentData {
         creator: string;
         certificate: string;
@@ -64,7 +68,6 @@ const Agents = () => {
 
     useEffect(() => {
         if (data) {
-            //setAgents((prevAgents) => [...prevAgents, data.toString()]);
             setIndex((prevIndex) => prevIndex + 1);
             setAddress(data.toString());
         }
@@ -73,33 +76,36 @@ const Agents = () => {
     useEffect(() => {
         if (agentData !== undefined) {
             console.log("Agent data:", agentData);
-            const parsedData: AgentData = {
-                creator: agentData[0].toString(),
-                certificate: agentData[1].toString(),
-                pair: agentData[2].toString(),
-                agentToken: agentData[3].toString(),
-                token: agentData[4][0].toString(),
-                tokenName: agentData[4][1].toString(),
-                _tokenName: agentData[4][2].toString(),
-                tokenTicker: agentData[4][3].toString(),
-                supply: parseInt(agentData[4][4].toString()),
-                price: parseInt(agentData[4][5].toString()),
-                marketCap: parseInt(toEther(agentData[4][6])), // todo: further convert to USD
-                liquidity: parseInt(agentData[4][7].toString()),
-                volume: parseInt(agentData[4][8].toString()),
-                volume24H: parseInt(agentData[4][9].toString()),
-                prevPrice: parseInt(agentData[4][10].toString()),
-                lastUpdated: new Date(parseInt(agentData[4][11].toString()) * 1000),
-                description: agentData[5].toString(),
-                image: agentData[6].toString(),
-                twitter: agentData[7].toString(),
-                telegram: agentData[8].toString(),
-                youtube: agentData[9].toString(),
-                website: agentData[10].toString(),
-                trading: agentData[11].valueOf(),
-                tradingOnUniSwap: agentData[12]?.valueOf(),
-            };
-            setAgentsData((prevAgentsData) => [...prevAgentsData, parsedData]);
+            const certificate = agentData[1].toString();
+            if (certificate !== "0x0000000000000000000000000000000000000000") {
+                const parsedData: AgentData = {
+                    creator: agentData[0].toString(),
+                    certificate: certificate,
+                    pair: agentData[2].toString(),
+                    agentToken: agentData[3].toString(),
+                    token: agentData[4][0].toString(),
+                    tokenName: agentData[4][1].toString(),
+                    _tokenName: agentData[4][2].toString(),
+                    tokenTicker: agentData[4][3].toString(),
+                    supply: parseInt(agentData[4][4].toString()),
+                    price: parseInt(agentData[4][5].toString()),
+                    marketCap: parseInt(toEther(agentData[4][6])), // todo: further convert to USD
+                    liquidity: parseInt(agentData[4][7].toString()),
+                    volume: parseInt(agentData[4][8].toString()),
+                    volume24H: parseInt(agentData[4][9].toString()),
+                    prevPrice: parseInt(agentData[4][10].toString()),
+                    lastUpdated: new Date(parseInt(agentData[4][11].toString()) * 1000),
+                    description: agentData[5].toString(),
+                    image: agentData[6].toString(),
+                    twitter: agentData[7].toString(),
+                    telegram: agentData[8].toString(),
+                    youtube: agentData[9].toString(),
+                    website: agentData[10].toString(),
+                    trading: agentData[11].valueOf(),
+                    tradingOnUniSwap: agentData[12]?.valueOf(),
+                };
+                setAgentsData((prevAgentsData) => [...prevAgentsData, parsedData]);
+            }
         }
     }, [agentData]);    
 
@@ -179,6 +185,28 @@ const Agents = () => {
         setFilteredAgents(agentsData);
     }, [agentsData]);
 
+    const handlePageChange = (newPage: number) => {
+        setCurrentPage(newPage);
+    };
+
+    const indexOfLastAgent = currentPage * agentsPerPage;
+    const indexOfFirstAgent = indexOfLastAgent - agentsPerPage;
+    const currentAgents = filteredAgents.slice(indexOfFirstAgent, indexOfLastAgent);
+    const totalPages = Math.ceil(filteredAgents.length / agentsPerPage);
+
+    const getPageNumbers = () => {
+        const pageNumbers = [];
+        const maxPagesToShow = 3;
+        const startPage = Math.max(1, currentPage - Math.floor(maxPagesToShow / 2));
+        const endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
+
+        for (let i = startPage; i <= endPage; i++) {
+            pageNumbers.push(i);
+        }
+
+        return pageNumbers;
+    };
+
     return (
         <section className="items-center justify-center min-h-screen m-6 lg:mx-2 xl:mx-10 2xl:mx-24">
             <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 lg:gap-6 xl:gap-10 2xl:gap-24 w-full">
@@ -193,19 +221,25 @@ const Agents = () => {
                 {/* Second Column */}
                 <div className="lg:col-span-3 w-full">
                     <div className="flex items-center justify-center h-16 mb-6">
-                        <AgentSearchBar onSearch={(query: string) => {
-                            setSearchQuery(query);
-                        }} onClear={() => {
-                            setSearchQuery("");
-                            setFilteredAgents(agentsData); // Reset to original agents list
-                        }} placeholder="Search Agent/Token" />
+                        <AgentSearchBar
+                            onSearch={(query: string) => {
+                                setSearchQuery(query);
+                            }}
+                            onClear={() => {
+                                setSearchQuery("");
+                                setFilteredAgents(agentsData); // Reset to original agents list
+                            }}
+                            onSearchButtonClick={() => {
+                                setCurrentPage(1);
+                            }}
+                            placeholder="Search Agent/Token"
+                        />
                     </div>
 
                     {/* Agent Cards */}
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-3 gap-6 w-full">
-                        {filteredAgents.length > 0 ? (
-                            filteredAgents
-                                .filter(agent => agent.certificate !== "0x0000000000000000000000000000000000000000")
+                        {currentAgents.length > 0 ? (
+                            currentAgents
                                 .map((agent, index) => (
                                     <AgentCard
                                         key={index}
@@ -229,6 +263,33 @@ const Agents = () => {
                                 No agents found
                             </div>
                         )}
+                    </div>
+
+                    {/* Pagination */}
+                    <div className="flex justify-center mt-6">
+                        <button
+                            onClick={() => handlePageChange(Math.max(1, currentPage - 1))}
+                            className={`px-4 py-2 mx-1 rounded-lg transition-all ${currentPage === 1 ? "bg-gray-100 text-gray-300 dark:bg-gray-800 dark:text-gray-600" : "bg-gray-200 text-gray-700 hover:bg-sparkyOrange-200 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-sparkyOrange-700"} `}
+                            disabled={currentPage === 1}
+                        >
+                            {<IconArrowLeft />}
+                        </button>
+                        {getPageNumbers().map((pageNumber) => (
+                            <button
+                                key={pageNumber}
+                                onClick={() => handlePageChange(pageNumber)}
+                                className={`px-4 py-2 mx-1 rounded-lg transition-all ${currentPage === pageNumber ? 'bg-sparkyOrange-500 text-white dark:bg-sparkyOrange-700' : 'hover:bg-sparkyOrange-200 bg-gray-200 text-gray-700 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-sparkyOrange-700'}`}
+                            >
+                                {pageNumber}
+                            </button>
+                        ))}
+                        <button
+                            onClick={() => handlePageChange(Math.min(totalPages, currentPage + 1))}
+                            className="px-4 py-2 mx-1 bg-gray-200 text-gray-700 rounded-lg hover:bg-sparkyOrange-200 transition-all dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-sparkyOrange-700"
+                            disabled={currentPage === totalPages}
+                        >
+                            {<IconArrowRight />}
+                        </button>
                     </div>
                 </div>
             </div>
