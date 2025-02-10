5a52ba4f91668d461e148129be1173c5c85cddde
Kevin Kyle Hendrick Baldo <30228996+kbaldo@users.noreply.github.com>
2025-02-11 00:02:03 +0800

    Create ForumEntry component

---
+++ app/components/ForumEntry.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { formatTimestamp, truncateHash } from "../lib/utils/formatting";
+import Image from "next/image";
+import blockies from "ethereum-blockies";
+import { useEffect, useRef, useState } from "react";
+import { updateImageSrc } from "../lib/utils/utils";
+import { IconLoader3 } from "@tabler/icons-react";
+import { cardProperties } from "../lib/utils/style/customStyles";
+
+interface ForumEntryProps {
+    id: string;
+    user: string;
+    message: string;
+    timestamp: Date;
+    agentCertificate: string;
+    agentName: string;
+    agentImage: string;
+}
+
+const ForumEntry: React.FC<ForumEntryProps> = ({
+    user,
+    message,
+    timestamp,
+    agentCertificate,
+    agentName,
+    agentImage,
+}) => {
+    const blockiesIcon = blockies.create({
+        seed: agentCertificate,
+        size: 16,
+        scale: 8,
+    });
+    const prevImageRef = useRef<string | null>(null);
+    const [imgSrc, setImgSrc] = useState(
+        `https://yellow-patient-hare-489.mypinata.cloud/ipfs/${agentImage}`
+    );
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        if (prevImageRef.current === agentImage) return;
+        prevImageRef.current = agentImage;
+        updateImageSrc(agentImage, blockiesIcon, setImgSrc, setIsLoading);
+    }, [agentImage, blockiesIcon, imgSrc]);
+
+    const headerProperties = "font-bold text-sparkyOrange-600 mx-1";
+
+    return (
+        <motion.div
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            className={cardProperties + " space-y-4"}>
+            {/* User Reply Area */}
+            <div className="flex items-center">
+                <div className="font-bold border-2 border-sparkyOrange-600 p-1 rounded-lg truncate">
+                    <h2 className={`${headerProperties} hidden md:block`}>{user}</h2>
+                    <h2 className={`${headerProperties} md:hidden`}>{truncateHash(user, 6)}</h2>
+                </div>
+                <span className="ml-auto">{formatTimestamp(timestamp)}</span>
+            </div>
+            <p>{message}</p>
+
+            {/* Agent Reply Area */}
+            <div className="flex flex-col gap-4">
+                <div className="bg-gray-100 dark:bg-gray-800 p-4 rounded-lg border-2 dark:border-gray-600 ">
+                    <div className="flex items-center mb-2 justify-between">
+                        <div className="flex items-center">
+                            <div className="relative w-14 h-14 flex-shrink-0 rounded-full overflow-hidden mr-4 md:block">
+                                {isLoading && (
+                                    <motion.div
+                                        className="absolute inset-0 flex items-center justify-center"
+                                        animate={{ rotate: 360 }}
+                                        transition={{
+                                            repeat: Infinity,
+                                            duration: 1,
+                                            ease: "linear",
+                                        }}
+                                    >
+                                        <IconLoader3 size={24} />
+                                    </motion.div>
+                                )}
+                                <motion.div
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: isLoading ? 0 : 1 }}
+                                    transition={{ duration: 0.5 }}
+                                    className="w-full h-full"
+                                >
+                                    <Image
+                                        src={imgSrc}
+                                        alt={"Card image"}
+                                        layout="fill"
+                                        objectFit="cover"
+                                        className="object-cover"
+                                        sizes="(max-width: 768px) 100vw, 33vw"
+                                        onLoad={() => setIsLoading(false)}
+                                    />
+                                </motion.div>
+                            </div>
+                            <div className="mr-2 font-bold border-2 border-sparkyOrange-600 p-1 rounded-lg">
+                                <h2 className={headerProperties}>
+                                    {agentName + " Agent"}
+                                </h2>
+                            </div>
+                        </div>
+                        <span className="ml-auto">{formatTimestamp(timestamp)}</span>
+                    </div>
+                    <p>{message}</p>
+                </div>
+            </div>
+        </motion.div>
+    );
+};
+
+export default ForumEntry;
