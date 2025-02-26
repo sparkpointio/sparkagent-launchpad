37c1afa0bf37af18c8ea1992ede43b938f1e1005
Kevin Kyle Hendrick Baldo <30228996+kbaldo@users.noreply.github.com>
2025-02-26 16:22:05 +0800

    Improve Hero component performance

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,22 +5,32 @@ import { bg_hero_dark } from '../lib/assets';
 import { arbitrumSepolia } from "thirdweb/chains";
 import { client } from '../client';
 import { ConnectButton } from 'thirdweb/react';
-import {CreateAgentForm} from "../components/CreateAgentForm";
+import { CreateAgentForm } from "../components/CreateAgentForm";
 import { buttonVariants } from '../components/variants/button-variants';
+import Image from 'next/image';
 
 const Hero = () => {
-    const account = useActiveAccount();
+  const account = useActiveAccount();
 
-    const customButtonStyles = {
-        label: "Create Agent",
-        className: "!font-[Rubik] !w-[192px] sm:w-48 py-3 !transition-all !duration-200 cursor-pointer hover:-translate-y-[0.25rem] hover:translate-x-[-0.25rem] !text-white !bg-black hover:bg-black hover:shadow-[0.25rem_0.25rem_#E5E7EB] active:translate-x-0 active:translate-y-0 active:shadow-none !rounded-full",
-    };
+  const customButtonStyles = {
+      label: "Create Agent",
+      className: "!font-[Rubik] !w-[192px] sm:w-48 py-3 !transition-all !duration-200 cursor-pointer hover:-translate-y-[0.25rem] hover:translate-x-[-0.25rem] !text-white !bg-black hover:bg-black hover:shadow-[0.25rem_0.25rem_#E5E7EB] active:translate-x-0 active:translate-y-0 active:shadow-none !rounded-full",
+  };
 
   return (
     <section className="text-center relative w-full h-[75vh] m:h-[60vh]">
-      <div className="bg-cover bg-center bg-no-repeat absolute top-0 left-0 w-full h-full z-0 bg-img-1" style={{ backgroundImage: `url(${bg_hero_dark.src})` }}></div>
-      {/* <div className="bg-cover bg-center bg-no-repeat absolute top-0 left-0 w-full h-full z-0 bg-img-2" style={{ backgroundImage: `url(${bg_hero_dark.src})` }}></div> */}
-      <div className="absolute top-0 left-0 w-full h-full z-1 bg-mask"></div>
+      <link rel="preload" as="image" href={bg_hero_dark.src} />
+      <div className="absolute top-0 left-0 w-full h-full z-0">
+        <Image
+          src={bg_hero_dark.src}
+          alt="Hero Background"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          priority
+        />
+      </div>
+      <div className="absolute top-0 left-0 w-full h-full z-1 bg-mask" />
 
       <div className='m-auto max-w-4xl px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center h-full relative z-2'>
         <h1 className="text-4xl xl:text-5xl 2xl:text-6xl leading-tight text-center mt-24 text-white">
